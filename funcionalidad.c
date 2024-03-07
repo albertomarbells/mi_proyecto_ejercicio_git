91a5f26c249b2fbbffcf800df551c552b655b5c3
alberto <albertomarbells@gmail.com>
2024-03-07 20:25:52 +0100

    Agrega nueva funcionalidad

---
+++ funcionalidad.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+
+{
+int x;
+printf("Escribe el numero sin decimales:\n");
+scanf("%d",&x);
+
+if (x%2==0)
+printf("El numero es par");
+
+else
+printf("El numero es impar");
+}
